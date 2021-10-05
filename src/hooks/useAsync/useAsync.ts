8dc8c3c9c00b4f44b0b53da9b808ec56204d32bd
Brandon <bran.ramirez.don@gmail.com>
2021-10-05 10:54:54 -0700

    v0.1.0

--- src/hooks/useAsync/useAsync.ts
+++ src/hooks/useAsync/useAsync.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState } from "react";
 
 export default function useAsync<FunctionReturn, Params extends unknown[]>({
   asyncFunction,
